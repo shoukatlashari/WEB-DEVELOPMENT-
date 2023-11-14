087115a920ab2fa1de4ed872542b737b5da6d2bb
shoukatlashari <muhammadshoukat892@gmail.com>
2023-11-14 10:00:31 +0500

    this is 14-nov work

---
+++ code.js
@@ -0,0 +1,27 @@
+ var day = 'sunday';
+ switch (day) {
+     case 'monday':
+         console.log('do something')
+         break;
+     case 'tuesday':
+         console.log('do something')
+         break;
+     case 'wednesday':
+         console.log('do something')
+         break;
+     case 'thursday':
+         console.log('do something')
+         break;
+     case 'friday':
+         console.log('do something')
+         break;
+     case 'saturday':
+         console.log('do something')
+         break;
+     case 'sunday':
+         console.log('do something')
+         break;
+     default:
+         console.log('do something')
+         break;
+ }
